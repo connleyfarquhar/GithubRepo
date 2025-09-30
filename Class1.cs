3a90bb6bcfa6a01df452820261f529f021e247b9
Connley <142690577+connleyfarquhar@users.noreply.github.com>
2025-09-30 10:47:30 +0100

    -m "initial commit"

--- Class1.cs
+++ Class1.cs
@@ -5,6 +5,7 @@ public class Class1
 	public Class1()
 	{
 		Console.WriteLine("Hello World, Welcome to my Github Repository Project!");
-		Console.WriteLine("I Just added this in Visual Studio!")
+		Console.WriteLine("I Just added this in Visual Studio!");
+		Console.WriteLine("This line added to show the staging area");
 	}
 }
