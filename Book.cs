9a88571ec1c13f13367bfca5a5416e1084c5940f
Connley <142690577+connleyfarquhar@users.noreply.github.com>
2025-09-30 12:11:22 +0100

    Created Book Class

---
+++ Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubRepo
+{
+	public class Book
+	{
+		public string Title { get; set; }
+		public List<string> Authors { get; set; }
+
+		public DateTime PublicationDate { get; set; }
+	}
+}
